d5b3aa4b36e105e569d2a04e09dbfd87b3a16dcf
Andrei2604 <andrejporodin@gmail.com>
2024-09-07 19:17:52 -0400

    all done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,39 +24,61 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  
-  return res.status(200).send(JSON.stringify(books, null, 4));
+  let promise = new Promise((resolve, reject) => {
+    resolve(JSON.stringify(books, null, 4));
+  });
+  promise.then((message) => {
+    res.status(200).send(message);
+  });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  const findedBook = books[isbn];
-  res.status(200).send(findedBook);
+  let promise = new Promise((resolve, reject) => {
+    const isbn = req.params.isbn;
+    resolve(books[isbn]);
+  })
+  
+  promise.then((message) => {
+    res.status(200).send(message);
+  });
+
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author;
-  let result = [];
-  Object.entries(books).forEach(([key, value]) => {
-    if (value.author === author){
-        result.push(value);
-    }
+  let promise = new Promise((resolve, reject) => {
+    const author = req.params.author;
+    let result = [];
+    Object.entries(books).forEach(([key, value]) => {
+        if (value.author === author){
+            result.push(value);
+        }
+    });
+    resolve(result);
   });
-  res.status(200).send(result);
+  
+  promise.then((message) => {
+    res.status(200).send(message);
+  });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  const title = req.params.title;
-  let result = [];
-  Object.entries(books).forEach(([key, value]) => {
-    if (value.title === title){
-        result.push(value);
-    }
+  let promise = new Promise((resolve, reject) => {
+    const title = req.params.title;
+    let result = [];
+    Object.entries(books).forEach(([key, value]) => {
+      if (value.title === title){
+          result.push(value);
+      }
+    });
+    resolve(result);
+  })
+ 
+  promise.then((message) => {
+    res.status(200).send(message);
   });
-  res.status(200).send(result);
 });
 
 //  Get book review
