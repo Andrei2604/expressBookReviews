498077a6e39708d2f9234828cc1f771815e04e77
Andrei2604 <andrejporodin@gmail.com>
2024-09-06 22:01:57 -0400

    fix bugs

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,24 +7,45 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
     const user_with_same_name = users.filter((user) => {
-        user.username === username
+       return user.username == username
     });
     return user_with_same_name.length === 0;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    const validusers = users.filter((user) => {
+       return user.username == username && user.password == password
+    });
+    
+    return validusers.length > 0;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password){
+    if(authenticatedUser(username, password)){
+        let token = jwt.sign({
+            data: password
+        },"access", {expiresIn: 60 * 60});
+
+        req.session.authorization = {token, username};
+        
+        res.status(200).json({message: "User successfully logged in"});
+    }
+    else{
+        res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
+  }
+  else{
+    res.status(404).json({ message: "Error logging in" });
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
+  
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
